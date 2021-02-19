ab42eb4f368bd5179b351bad44288c937b637caa
Michael Iacono <iaconom821@gmail.com>
2021-02-19 21:54:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,17 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(file)
+  library = YAML.load_file(file)
+  library_hash = library.each_with_object({}) do |(key, value), new_hash|
+    new_hash[key] = {
+      'english' => value[0],
+      'japanese' => value[1]
+    }
+    end
+  library_hash
+  binding.pry
 end
 
 def get_japanese_emoticon
